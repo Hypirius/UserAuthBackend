9e582225361474b9fb800fa8f01e99815911eafc
Hypirius <zuhayerhossain25@gmail.com>
2026-04-13 00:29:42 +0600

    feat: connects to DB and subscribes to graceful exit system

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import app from './app.js';
 import config from './config/env.js';
+import connectDB from './db/connectDB.js';
+import setGracefulExits from './utils/setGracefulExits.js';
+
+await connectDB(config.DB_URL, config.DB_NAME);
+setGracefulExits();
 
 app.listen(config.PORT, () => {
   console.log(`Connected to server on PORT ${config.PORT}`);

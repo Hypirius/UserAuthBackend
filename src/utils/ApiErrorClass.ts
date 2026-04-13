89a05d4ee1647322bd993d370ea6957bfe09fdaf
Hypirius <zuhayerhossain25@gmail.com>
2026-04-13 23:06:22 +0600

    feat: Add support for more detailed error message

--- src/utils/ApiErrorClass.ts
+++ src/utils/ApiErrorClass.ts
@@ -3,10 +3,15 @@ import config from '../config/env.js';
 class ApiError extends Error {
   success: boolean;
   statusCode: number;
-  constructor(statusCode: number, errorMessage: string) {
+  details?: string;
+  constructor(statusCode: number, errorMessage: string, details?: string) {
     super(errorMessage);
     this.success = false;
     this.statusCode = statusCode;
+
+    if (details) {
+      this.details = details;
+    }
 
     if (config.NODE_ENV !== 'development') delete this.stack;
   }

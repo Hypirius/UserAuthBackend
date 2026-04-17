7b19414c5371b481b7a9076e3e4fe0f4503d6ac2
Hypirius <zuhayerhossain25@gmail.com>
2026-04-17 16:25:16 +0600

    chore: built multer config for multer middleware usage for image saving properties and location

---
+++ src/config/multer.config.ts
@@ -0,0 +1,22 @@
+import { type Request } from 'express';
+import crypto from 'node:crypto';
+import multer, { type FileFilterCallback } from 'multer';
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, '../../public/temp');
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + crypto.randomUUID() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+  if (['image/jpeg', 'image/png'].includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(new Error('Only PNG or JPG are allowed'));
+  }
+};
+
+export const upload = multer({ storage, fileFilter, limits: { fileSize: 1024 * 4096 } });

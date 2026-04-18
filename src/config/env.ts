c533862bc438170ec027988595e709e6ea283326
Hypirius <zuhayerhossain25@gmail.com>
2026-04-18 22:37:10 +0600

    chore: change env system to zod validation to simplify env addition and maintenance

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,20 +2,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { config as dotenvConfig } from 'dotenv';
-
-interface configType {
-  PORT: number;
-  DB_NAME: string;
-  DB_URL: string;
-  NODE_ENV: string;
-  __dirname: string;
-  __filename: string;
-  CORS_ORIGIN: string;
-  JWT_ACCESS_SECRET: string;
-  JWT_REFRESH_SECRET: string;
-  JWT_REFRESH_DURATION_DAYS: number;
-  JWT_ACCESS_DURATION_MINS: number;
-}
+import * as z from 'zod';
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -26,24 +13,22 @@ dotenvConfig({
   path: path.join(__dirname, '../../', `.env.${NODE_ENV}.local`),
 });
 
-const PORT = Number(process.env.PORT) || 3000;
+const validateEnvSchema = z.object({
+  PORT: z.coerce.number(),
+  DB_NAME: z.string(),
+  DB_URL: z.string(),
+  NODE_ENV: z.string(),
+  CORS_ORIGIN: z.string(),
+  JWT_ACCESS_SECRET: z.string(),
+  JWT_REFRESH_SECRET: z.string(),
+  JWT_REFRESH_DURATION_DAYS: z.coerce.number().min(1),
+  JWT_ACCESS_DURATION_MINS: z.coerce.number().min(30),
+  CLOUDINARY_API_URL: z.url(),
+  CLOUDINARY_API_NAME: z.string(),
+  CLOUDINARY_API_KEY: z.coerce.string(),
+  CLOUDINARY_API_SECRET: z.string(),
+});
 
-const config: configType = {
-  PORT,
-  DB_NAME: process.env.DB_NAME || 'test',
-  DB_URL: process.env.DB_URL || 'mongodb://localhost:27017',
-  NODE_ENV,
-  __dirname,
-  __filename,
-  CORS_ORIGIN: process.env.CORS_ORIGIN || `https://localhost:${PORT}`,
-  JWT_ACCESS_SECRET:
-    process.env.JWT_ACCESS_SECRET ||
-    Buffer.from('development-access-secret', 'utf8').toString('base64'),
-  JWT_REFRESH_SECRET:
-    process.env.JWT_ACCESS_SECRET ||
-    Buffer.from('development-refresh-secret', 'utf8').toString('base64'),
-  JWT_REFRESH_DURATION_DAYS: Number(process.env.JWT_REFRESH_DURATION_DAYS) || 7,
-  JWT_ACCESS_DURATION_MINS: Number(process.env.JWT_ACCESS_DURATION_MINS) || 30,
-};
+const env = validateEnvSchema.parse(process.env);
 
-export default Object.freeze(config);
+export default Object.freeze(env);
